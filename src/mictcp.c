4334d91e7f59d81d38b72492a7d0ca32a5f535a4
Kanabenki <lucien.menassol@gmail.com>
2018-03-26 08:16:22 +0200

    Initial commit

---
+++ src/mictcp.c
@@ -0,0 +1,146 @@
+#include <mictcp.h>
+#include <api/mictcp_core.h>
+#include <string.h>
+
+#define MAX_SOCKET 10
+#define TIMEOUT 10000
+#define LOSS_RATE 0
+
+
+enum start_mode current_mode = SERVER;
+static struct mic_tcp_sock internal_sock[MAX_SOCKET];
+static int curr_socket_nb = 0;
+
+/*
+ * Permet de créer un socket entre l’application et MIC-TCP
+ * Retourne le descripteur du socket ou bien -1 en cas d'erreur
+ */
+int mic_tcp_socket(start_mode sm)
+{
+   printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
+   if (initialize_components(sm) == -1) {
+       return -1;
+   } /* Appel obligatoire */
+    set_loss_rate(LOSS_RATE);
+    current_mode = sm;
+    if (curr_socket_nb >= MAX_SOCKET) {
+        return -1;
+    }
+    internal_sock[curr_socket_nb].fd = curr_socket_nb;
+    curr_socket_nb++;
+    return curr_socket_nb-1;
+}
+
+/*
+ * Permet d’attribuer une adresse à un socket.
+ * Retourne 0 si succès, et -1 en cas d’échec
+ */
+int mic_tcp_bind(int socket, mic_tcp_sock_addr addr)
+{
+   printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
+   if (socket < 0 || socket >= curr_socket_nb) {
+       return -1;
+   }
+   internal_sock[socket].addr = addr;
+   return 0;
+}
+
+/*
+ * Met le socket en état d'acceptation de connexions
+ * Retourne 0 si succès, -1 si erreur
+ */
+int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
+{
+    struct mic_tcp_pdu pdu;
+    struct mic_tcp_sock_addr;
+
+    printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
+    /* do {
+        IP_recv(pdu, addr, TIMEOUT);
+    } while (!pdu.header.syn); */
+    return 0; //TODO CHANGE
+}
+
+/*
+ * Permet de réclamer l’établissement d’une connexion
+ * Retourne 0 si la connexion est établie, et -1 en cas d’échec
+ */
+int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
+{
+    printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
+    return 0; //TODO CHANGE
+}
+
+/*
+ * Permet de réclamer l’envoi d’une donnée applicative
+ * Retourne la taille des données envoyées, et -1 en cas d'erreur
+ */
+int mic_tcp_send (int socket, char* mesg, int mesg_size)
+{
+    struct mic_tcp_pdu pdu;
+    struct mic_tcp_sock_addr addr;
+    addr.ip_addr = "localhost";
+    addr.ip_addr_size = strlen("localhost");
+    addr.port = 10000;
+
+    printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
+
+    if (socket < 0 || socket >= curr_socket_nb) {
+        return -1;
+    }
+
+    memset(&pdu, 0, sizeof(pdu));
+    pdu.header.source_port = 10000;
+    pdu.header.dest_port = 10000; //TODO change
+    //TODO header complet
+    pdu.payload.data = mesg;
+    pdu.payload.size = mesg_size;
+
+    IP_send(pdu, addr);    
+    return 0;
+}
+
+/*
+ * Permet à l’application réceptrice de réclamer la récupération d’une donnée
+ * stockée dans les buffers de réception du socket
+ * Retourne le nombre d’octets lu ou bien -1 en cas d’erreur
+ * NB : cette fonction fait appel à la fonction app_buffer_get()
+ */
+int mic_tcp_recv (int socket, char* mesg, int max_mesg_size)
+{
+    struct mic_tcp_payload payload;
+
+    printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
+    if (socket < 0 || socket >= curr_socket_nb) {
+        return -1;
+    }
+
+    payload.data = mesg;
+    payload.size = max_mesg_size;
+    return app_buffer_get(payload);
+}
+
+/*
+    IP_recv(&pdu, &addr, TIMEOUT);
+    IP_recv(&pdu, &addr, TIMEOUT);
+ * Permet de réclamer la destruction d’un socket.
+ * Engendre la fermeture de la connexion suivant le modèle de TCP.
+ * Retourne 0 si tout se passe bien et -1 en cas d'erreur
+ */
+int mic_tcp_close (int socket)
+{
+    printf("[MIC-TCP] Appel de la fonction :  "); printf(__FUNCTION__); printf("\n");
+    return 0;
+}
+
+/*
+ * Traitement d’un PDU MIC-TCP reçu (mise à jour des numéros de séquence
+ * et d'acquittement, etc.) puis insère les données utiles du PDU dans
+ * le buffer de réception du socket. Cette fonction utilise la fonction
+ * app_buffer_put().
+ */
+void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_sock_addr addr)
+{
+    printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
+    app_buffer_put(pdu.payload);
+}
